3f49006a18cdba679e417b23fe0f2d246b389c13
letsgilit <wjdrlf5781@naver.com>
2024-06-23 23:46:54 +0900

    fetch"Add 제외영양소검색 modify DBUpdate" 제외영양소 검색을 위한 기능추가 FoodNutrient 테이블 삽입시 영양소 0인값 제외 main.js/shop.js 분리 상세 음식 페이지 js 수정

---
+++ src/test/java/com/example/nutri_well/db/Test1.java
@@ -0,0 +1,40 @@
+package com.example.nutri_well.db;
+
+import com.example.nutri_well.dto.FoodResponseDTO;
+import com.example.nutri_well.entity.Food;
+import com.example.nutri_well.repository.FoodRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+
+public class Test1 {
+    @Autowired
+    private FoodRepository foodRepository;
+    @Test
+    public void test(){
+        List<String> list = new ArrayList<>();
+        list.add("탄수화물");
+        list.add("단백질");
+
+        Page<Food> list2 = foodRepository.findAllByNutrientsNotIn("%마카롱%", list, Pageable.unpaged());
+        List<FoodResponseDTO> list3 =  list2.stream().map(FoodResponseDTO::of).toList();
+
+        Page<Food> DFDF = foodRepository.findByNameContaining("마카롱", Pageable.unpaged());
+        List<FoodResponseDTO> list4 =DFDF.stream().map(FoodResponseDTO::of).toList();
+
+        System.out.println(list3.size()+","+list3.size()/10);
+        System.out.println(list4.size()+","+list4.size()/10);
+
+    }
+}
