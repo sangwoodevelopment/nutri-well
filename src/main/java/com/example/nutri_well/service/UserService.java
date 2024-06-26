b5b5b36c875a4c308d77a9c52d5326c3f40f06fd
nrg5477 <nrg5477@naver.com>
2024-06-26 13:47:20 +0900

    myPage 회원정보 수정 기능 구현

--- src/main/java/com/example/nutri_well/service/UserService.java
+++ src/main/java/com/example/nutri_well/service/UserService.java
@@ -20,4 +20,14 @@ public class UserService {
     public void save(User user) {
         userRepository.save(user);
     }
+
+    public void updateUser(User user) {
+        // 사용자 정보를 업데이트합니다.
+        userRepository.save(user);
+    }
+
+    public User getCurrentUser(String email) {
+        // 현재 로그인한 사용자 정보를 이메일로 가져옵니다.
+        return userRepository.findByEmail(email).orElse(null);
+    }
 }
