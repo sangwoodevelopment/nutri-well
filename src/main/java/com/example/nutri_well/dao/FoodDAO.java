ec10b7f1815b4fac425ef215d5a219af0ae4dc8e
sangwoo-00 <saka_si@naver.com>
2024-06-27 17:46:27 +0900

    Merge remote-tracking branch 'origin/order1' into order1

--- src/main/java/com/example/nutri_well/dao/FoodDAO.java
+++ src/main/java/com/example/nutri_well/dao/FoodDAO.java
@@ -3,10 +3,24 @@ package com.example.nutri_well.dao;
 import com.example.nutri_well.entity.Food;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface FoodDAO {
     Page<Food> searchByFoodName(String name, Pageable pageable);
+
     Page<Food> searchByCategoryId(Long categoryid, Pageable pageable);
+
     int getTotalPages(Page<Food> page);
+
     Food findByName(String name);
+
+    Page<Food> findAllByNutrientsNotIn(@Param("name") String foodname, @Param("names") List<String> names, Pageable pageable);
+
+    Page<Food> findAllByNutrientsNotIn(@Param("category") Long categoryid, @Param("names") List<String> names, Pageable pageable);
+
+    Food findById(Long foodId);
+    Food findByFoodCode(String foodcode);
 }
--- src/main/java/com/example/nutri_well/model/User.java
+++ src/main/java/com/example/nutri_well/model/User.java
@@ -1,54 +1,53 @@
 package com.example.nutri_well.model;
 
+import com.example.nutri_well.entity.BookMark;
+import com.example.nutri_well.entity.Food;
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import java.util.Date;
+import java.util.List;
 
 @Entity
 @Table(name = "USERS")
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 public class User {
     @Id
     @GeneratedValue
-    private Long id;
+    private Long userId;
 
     @Column(nullable = false)
     private String username;
 
-    @Column(nullable = false)
-    private String email; //=> 로그인할때는 email + password . 중복된 값이 허용됨. => userId가 email 이 userId 가 되고 UNIQUE
-
-    //email
+    @Column(nullable = false, unique = true)
+    private String email; //=> 로그인할때는 email + password . UNIQUE
 
     @Column(nullable = false)
     private String gender; //수정가능
 
     @Column
-    private float weight; //수정가능
+    private Float weight; //수정가능
 
     @Column
-    private String birth; //수정가능
+    private Date birth; //수정가능
 
     @Column
     private String tel; //수정가능
 
     @Column(nullable = false)
-    private boolean state; //가입, 탈퇴여부
+    private boolean state = true; //가입, 탈퇴여부
 
     @Column
-    private float height; //수정가능
+    private Float height; //수정가능
 
     @Column
-    private int basel_metabolism; //수정가능
+    private int baselMetabolism; //수정가능
 
     @Column(nullable = false)
-    private String password; //수정가능 => 일반사용자가 email 을 입력하지 못하게 해서 email 있으면 소셜로그인, 소셜로그인은 비밀번호 추가,수정 불가.
+    private String password;
 
     @Column
     private String picture; //수정가능
@@ -56,9 +55,10 @@ public class User {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Role role;
+    private int basel_metabolism;
 
     @Builder
-    public User(String name, String email, String password, String picture, Role role, String gender, boolean state, String birth, String tel) {
+    public User(String name, String email, String password, String picture, Role role, String gender, boolean state, Date birth, String tel) {
         this.username = name;
         this.email = email;
         this.password = password;
@@ -69,11 +69,17 @@ public class User {
         this.birth = birth;
         this.tel = tel;
     }
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<myCalendar> calendars;
+
+    @ToString.Exclude
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<BookMark> bookmark;
 
     public User update(String name, String picture) {
         this.username = name;
         this.picture = picture;
-
         return this;
     }
 
